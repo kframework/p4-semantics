a243f5ff3aab4635df2b314b9532f302156f355f
Ali Kheradmand <a.i.kheradmand@gmail.com>
2018-03-29 06:09:41 -0400

    only 4 sync points, add table constraints, does not go through

---
+++ tranlation-validation/simple/4srun.sh
@@ -0,0 +1,1 @@
+keq -v --debug --def0 common-kompiled --def1 impp-kompiled --def2 impp-kompiled --prove1 impp2-4syncpoints-spec.k --prove2 impp-4syncpoints-spec.k --smt_prelude ../../verification/load-balancer/list.smt2
---
+++ tranlation-validation/simple/skompile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -x
+rm -r impp-kompiled common-kompiled
+kompile impp.k
+kompile common.k
