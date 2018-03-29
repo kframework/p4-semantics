cc95dca8973276804f646619e1edf21af77dffdf
Ali Kheradmand <a.i.kheradmand@gmail.com>
2018-03-29 06:10:57 -0400

    imppp-4syncpoints with imppp.k is the correct spec, proof goes through

---
+++ tranlation-validation/simple/4ssrun.sh
@@ -0,0 +1,1 @@
+keq -v --debug --def0 common-kompiled --def1 imppp-kompiled --def2 imppp-kompiled --prove1 imppp2-4syncpoints-spec.k --prove2 imppp-4syncpoints-spec.k --smt_prelude ../../verification/load-balancer/list.smt2
---
+++ tranlation-validation/simple/sskompile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -x
+rm -r imppp-kompiled common-kompiled
+kompile imppp.k
+kompile common.k
