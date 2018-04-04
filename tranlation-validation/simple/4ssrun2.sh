5c880983239d2e8763673560eeab685e7b0f8bc1
Ali Kheradmand <a.i.kheradmand@gmail.com>
2018-04-04 01:51:34 -0400

    add missing file

---
+++ tranlation-validation/simple/4ssrun2.sh
@@ -0,0 +1,1 @@
+keq -v --debug --def0 common-kompiled --def1 imppp-kompiled --def2 imppp-kompiled --prove1 2imppp2-4syncpoints-spec.k --prove2 2imppp-4syncpoints-spec.k --smt_prelude ../../verification/load-balancer/list.smt2
